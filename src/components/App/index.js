70570cceb15eda0fc895e9aaf6a06581970cd6f6
LaraMi92 <laraminski@hotmail.fr>
2020-12-16 09:50:02 +0100

    ajout logo github

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 
 // == Import
 import './styles.scss';
+import gitLogo from 'src/assets/images/logo-github.png';
 import SearchBar from 'src/components/SearchBar';
 import Message from 'src/components/Message';
 import ReposResults from 'src/components/ReposResults';
@@ -10,6 +11,7 @@ import ReposResults from 'src/components/ReposResults';
 // == Composant
 const App = () => (
   <div className="app">
+    <img src={gitLogo} alt="github logo" />
     <SearchBar />
     <Message />
     <ReposResults />
